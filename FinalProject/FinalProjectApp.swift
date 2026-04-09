40208fb99b543c55e6f236a9f44b28ea93532ee5
Carlos Fletes <fletesc07@gmail.com>
2026-04-09 11:23:06 -0700

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Carlos Fletes on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
