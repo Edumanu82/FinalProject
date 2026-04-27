ad90be9dfbc15b5e536fae438daef8e0f63c6542
Carlos_Fletes <fletesc07@gmail.com>
2026-04-27 13:45:28 -0700

    Adds the ability to change username and updates right away

---
+++ FinalProject/EditProfileScreen.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileScreen.swift
+//  FinalProject
+//
+//  Created by Carlos Fletes on 4/27/26.
+//
+
+import SwiftUI
+
+struct EditProfileScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileScreen()
+}
